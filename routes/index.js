7d585814e47028b900383a42cfb256ef47964e2b
potlitel <potlitel@gmail.com>
2022-04-07 21:04:48 -0400

    Se adiciona carpeta para almacenar collección de postman para testing correspondiente a api.

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ const escalas = require('../routes/Escala-Route');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({
+  /*res.json({
     message: "API - 👋🌎🌍🌏",
-  });
+  });*/
+    res.status(200).send("<html><div id='welcome'>Welcome to API - HomePage 🙌👋🌎⭐️👏☀️🚀✨</div></html>");
 });
 
 router.use(escalas.path, escalas.router);
