b9b457880f48f2100b19871641557bb7b5886400
potlitel <potlitel@gmail.com>
2022-04-18 16:44:15 -0400

    Se adiciona validationMiddleware a nomenclador ObjetivoGeológico.

--- routes/ObjetivoGeologico-Route.js
+++ routes/ObjetivoGeologico-Route.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/ObjetivoGeologico-Controller");
 
+const validationOG = require('../validations/ObjetivoGeologicoValidation')
+const {
+  validate
+} = require('../middlewares/validationMiddleware')
 // GET ALL
 router.get("/", Controller.get_all);
 
 // POST
-router.post("/", Controller.create);
+router.post("/", validate(validationOG.objetivoGeologico),Controller.create);
 
 // GET SINGLE
 router.get("/:id", Controller.get);
 
 //UPDATE
-router.put("/:id", Controller.update);
+router.put("/:id", validate(validationOG.objetivoGeologico),Controller.update);
 
 // DELETE
 router.delete("/:id", Controller.delete);
