c99f7103f1d079cb1b083578c09b6097b7329424
potlitel <potlitel@gmail.com>
2022-04-18 14:52:02 -0400

    Se adiciona validationMiddleware a nomenclador EstadoFactura.

--- routes/EstadoFactura-Route.js
+++ routes/EstadoFactura-Route.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/EstadoFactura-Controller");
 
+const validationEstadoFactura = require('../validations/EstadoFacturaValidation')
+const {
+  validate
+} = require('../middlewares/validationMiddleware')
 // GET ALL
 router.get("/", Controller.get_all);
 
 // POST
-router.post("/", Controller.create);
+router.post("/", validate(validationEstadoFactura.estadoFactura),Controller.create);
 
 // GET SINGLE
 router.get("/:id", Controller.get);
 
 //UPDATE
-router.put("/:id", Controller.update);
+router.put("/:id", validate(validationEstadoFactura.estadoFactura),Controller.update);
 
 // DELETE
 router.delete("/:id", Controller.delete);
---
+++ validations/EstadoFacturaValidation.js
@@ -0,0 +1,16 @@
+const Joi = require("joi");
+
+exports.estadoFactura = Joi.object()
+  .keys({
+    estado: Joi.string()
+      .min(5)
+      .max(10)
+      .required()
+      .messages({
+      'string.base': `El campo "estado" debe ser de tipo 'text'`,
+      'string.empty': `El campo "estado" no puede estar vacío`,
+      'string.min': `El campo "estado" debe tener un mínimo de longitud de {#limit} caracteres`,
+      'string.max': `El campo "estado" debe tener un máximo de longitud de {#limit} caracteres`,
+      'any.required': `El campo "estado" en un campo requerido`
+    })
+  });
