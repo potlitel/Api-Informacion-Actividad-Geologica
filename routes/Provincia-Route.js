49fdcec38cfcbc43eb45e75b07ae041d7a4df4e4
potlitel <potlitel@gmail.com>
2022-04-18 16:59:13 -0400

    Se adiciona validationMiddleware a nomenclador Provincia.

--- routes/Provincia-Route.js
+++ routes/Provincia-Route.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/Provincia-Controller");
 
+const validationProvincia = require('../validations/ProvinciaValidation')
+const {
+  validate
+} = require('../middlewares/validationMiddleware')
 // GET ALL
 router.get("/", Controller.get_all);
 
 // POST
-router.post("/", Controller.create);
+router.post("/", validate(validationProvincia.provincia),Controller.create);
 
 // GET SINGLE
 router.get("/:id", Controller.get);
 
 //UPDATE
-router.put("/:id", Controller.update);
+router.put("/:id", validate(validationProvincia.provincia),Controller.update);
 
 // DELETE
 router.delete("/:id", Controller.delete);
