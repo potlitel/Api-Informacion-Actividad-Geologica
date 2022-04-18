06a312b4c212803a085bb934c312747650a62529
potlitel <potlitel@gmail.com>
2022-04-18 17:10:01 -0400

    Se adiciona validationMiddleware a nomenclador Rol.

--- routes/Rol-Route.js
+++ routes/Rol-Route.js
@@ -2,17 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/Rol-Controller");
 
+const validationRol = require('../validations/RolValidation')
+const {
+  validate
+} = require('../middlewares/validationMiddleware')
 // GET ALL
 router.get("/", Controller.get_all);
 
 // POST
-router.post("/", Controller.create);
+router.post("/", validate(validationRol.rol),Controller.create);
 
 // GET SINGLE
 router.get("/:id", Controller.get);
 
 //UPDATE
-router.put("/:id", Controller.update);
+router.put("/:id", validate(validationRol.rol),Controller.update);
 
 // DELETE
 router.delete("/:id", Controller.delete);
