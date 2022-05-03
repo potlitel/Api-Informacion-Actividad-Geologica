1783d68f7b530d7a1daba05259831735ee0b73db
potlitel <potlitel@gmail.com>
2022-05-03 10:25:47 -0400

    Ajuste menor en UI de componente mainView.jsx.

--- frontend/nomenclatorsManager-ActGeo/src/components/mainView.jsx
+++ frontend/nomenclatorsManager-ActGeo/src/components/mainView.jsx
@@ -26,6 +26,7 @@ export default function GrupoMineralesList() {
         <div class="g6">Quisque lectus purus, pellentesque et vestibulum sit amet, sollicitudin nec nisi. Integer at risus dui, et sodales nisi. Nulla iaculis imperdiet magna et malesuada. Morbi accumsan lectus et velit porta vel aliquam dolor egestas. Curabitur tincidunt, sapien a elementum aliquam, metus arcu bibendum justo, at blandit tellus sapien vel turpis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus suscipit orci eget mi volutpat euismod.</div>
         
         <div class="clear"></div>
+        <div >
         <div class="g3">
           <Box bg='tomato' borderWidth='1px' borderRadius='lg' overflow='hidden' w='100%' p={4} color='white'>
             <img src={logo3} width="300" height="300" alt="CoolBrand" />
@@ -45,7 +46,8 @@ export default function GrupoMineralesList() {
           <Box bg='tomato' borderWidth='1px' borderRadius='lg' overflow='hidden' w='100%' p={4} color='white'>
             <img src={logo6} width="300" height="300" alt="CoolBrand" />
           </Box>
-        </div>
+          </div>
+          </div>
         <div class="clear"></div>
 
         <div class="g4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis lacus in metus tempor fringilla id eu nisi.</div>
@@ -53,10 +55,19 @@ export default function GrupoMineralesList() {
         <div class="g4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis lacus in metus tempor fringilla id eu nisi.</div>
         <div class="clear">&nbsp;</div>
 
-        <div class="g8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis lacus in metus tempor fringilla id eu nisi. Quisque lectus purus, pellentesque et vestibulum sit amet, sollicitudin nec nisi. Integer at risus dui, et sodales nisi. Nulla iaculis imperdiet magna et malesuada. Morbi accumsan lectus et velit porta vel aliquam dolor egestas. Curabitur tincidunt, sapien a elementum aliquam, metus arcu bibendum justo, at blandit tellus sapien vel turpis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus suscipit orci eget mi volutpat euismod. Suspendisse volutpat vestibulum magna et lobortis.</div>
+        <div class="g8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis lacus in metus tempor fringilla id eu nisi.
+          Quisque lectus purus, pellentesque et vestibulum sit amet, sollicitudin nec nisi. Integer at risus dui, et sodales nisi. Nulla
+          iaculis imperdiet magna et malesuada. Morbi accumsan lectus et velit porta vel aliquam dolor egestas. Curabitur tincidunt, sapien a
+          elementum aliquam, metus arcu bibendum justo, at blandit tellus sapien vel turpis. Cum sociis natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. Phasellus suscipit orci eget mi volutpat euismod. Suspendisse volutpat vestibulum magna
+          et lobortis.Curabitur tincidunt, sapien a
+          elementum aliquam, metus arcu bibendum justo, at blandit tellus sapien vel turpis. Cum sociis natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. Phasellus suscipit orci eget mi volutpat euismod. Suspendisse volutpat vestibulum magna
+          et lobortis.
+        </div>
         <div class="g4">
-        <Box bg='tomato' borderWidth='1px' borderRadius='lg' overflow='hidden' w='100%' p={4} color='white'>
-            <img src={logo2} width="300" height="300" alt="CoolBrand" />
+          <Box bg='tomato' borderWidth='1px' borderRadius='lg' overflow='hidden' w='100%' p={6} color='white'>
+            <img src={logo2} width="300" height="300" alt="CoolBrand"/>
         </Box>
         </div>
         <div class="clear"></div>
