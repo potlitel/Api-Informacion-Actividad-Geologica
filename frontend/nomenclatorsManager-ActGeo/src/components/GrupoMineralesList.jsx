316233911b09033511cf07406f42491c5efca306
potlitel <potlitel@gmail.com>
2022-06-19 18:41:56 -0400

    Se configura con CDN framework TailwindCss.

--- frontend/nomenclatorsManager-ActGeo/src/components/GrupoMineralesList.jsx
+++ frontend/nomenclatorsManager-ActGeo/src/components/GrupoMineralesList.jsx
@@ -91,15 +91,31 @@ export default function GrupoMineralesList() {
       <div class="clear">&nbsp;</div>
 
       <div class="gp33">
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using 'Content here, content here', making it
-        look like readable English. Many desktop publishing packages and web
-        page editors now use Lorem Ipsum as their default model text, and a
-        search for 'lorem ipsum' will uncover many web sites still in their
-        infancy. Various versions have evolved over the years, sometimes by
-        accident, sometimes on purpose (injected humour and the like).
+        <div class="mx-6">
+        <h1 class="my-6 text-3xl">My Course</h1>
+        <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+          <div class="flex justify-center md:justify-end -mt-16">
+            <img
+              class="w-20 h-20 object-cover rounded-full border-2 border-indigo-500"
+              src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+            />
+          </div>
+          <div>
+            <h2 class="text-gray-800 text-3xl font-semibold">Design Tools</h2>
+            <p class="mt-2 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              dolores deserunt ea doloremque natus error, rerum quas odio
+              quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus
+              consequuntur!
+            </p>
+          </div>
+          <div class="flex justify-end mt-4">
+            <a href="#" class="text-xl font-medium text-indigo-500">
+              John Doe
+            </a>
+          </div>
+        </div>
+      </div>
       </div>
       <div class="gp33">
         Contrary to popular belief, Lorem Ipsum is not simply random text. It
